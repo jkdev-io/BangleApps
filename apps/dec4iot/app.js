6fd24ba8ee1043307210393ec0c8c590918a2eaa
jkampich1411 <me@jkdev.run>
2023-04-02 20:51:21 +0200

    still not done...

--- apps/dec4iot/app.js
+++ apps/dec4iot/app.js
@@ -7,10 +7,10 @@ g.reset().clearRect(Bangle.appRect);
 http.post("https://banglejs-dec4iot.jkdev.workers.dev/connectivitycheck", {}, {"this-is-a-header": "from-a-post-request"}).then(data => {
   let json = JSON.parse(data.resp);
   
-  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Method: ${json.method}`, g.getWidth() / 2, g.getHeight() / 2 + 70);
-  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`AS: ${json.as}`, g.getWidth() / 2, g.getHeight() / 2 + 50);
-  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Headers: ${json.headers}`, g.getWidth() / 2, g.getHeight() / 2 + 30);
-  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Colocation: ${json.colo}`, g.getWidth() / 2, g.getHeight() / 2 + 10);
+  g.setFont("Vector", 15).setFontAlign(0, 0).drawString(`Method: ${json.method}`, g.getWidth() / 2, g.getHeight() / 2 + 70);
+  g.setFont("Vector", 15).setFontAlign(0, 0).drawString(`AS: ${json.as}`, g.getWidth() / 2, g.getHeight() / 2 + 50);
+  g.setFont("Vector", 15).setFontAlign(0, 0).drawString(`Headers: ${JSON.parse(json.headers)}`, g.getWidth() / 2, g.getHeight() / 2 + 30);
+  g.setFont("Vector", 15).setFontAlign(0, 0).drawString(`Colocation: ${json.colo}`, g.getWidth() / 2, g.getHeight() / 2 + 10);
 
 });
 
