d2f317983f28fcd86dbb01ada326aadb658e376c
jkampich1411 <me@jkdev.run>
2023-04-02 20:02:58 +0200

    list test data

--- apps/dec4iot/app.js
+++ apps/dec4iot/app.js
@@ -2,19 +2,17 @@ const http = require('dec4iot_lib_http');
 
 // clear the screen
 g.clear();
-
-
-// redraw the screen
-function draw(text) {
-  g.reset().clearRect(Bangle.appRect);
-  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(text, g.getWidth() / 2, g.getHeight() / 2 + 30);
-}
+g.reset().clearRect(Bangle.appRect);
 
 http.post("https://banglejs-dec4iot.jkdev.workers.dev/connectivitycheck", {}, {"this-is-a-header": "from-a-post-request"}).then(data => {
-  draw(data.resp);
-});
+  let json = JSON.stringify(data.resp);
+  
+  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Method: ${json.method}`, g.getWidth() / 2, g.getHeight() / 2 + 70);
+  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`AS: ${json.as}`, g.getWidth() / 2, g.getHeight() / 2 + 50);
+  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Headers: ${json.headers}`, g.getWidth() / 2, g.getHeight() / 2 + 30);
+  g.setFont("Vector", 10).setFontAlign(0, 0).drawString(`Colocation: ${json.colo}`, g.getWidth() / 2, g.getHeight() / 2 + 10);
 
-draw("Hello Lads");
+});
 
 // Load widgets
 Bangle.loadWidgets();
