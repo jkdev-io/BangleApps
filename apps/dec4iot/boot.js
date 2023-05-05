c45561cb56c41caba86e58baa915b8fa3c84de5e
Jakob <jakob.kampichler@gmail.com>
2023-05-05 22:51:40 +0200

    yesyes

--- apps/dec4iot/boot.js
+++ apps/dec4iot/boot.js
@@ -1,8 +1,13 @@
 const http = require('dec4iot_lib_http');
 const sensors = require('dec4iot_lib_sensor');
 
-setInterval(() => {
-    sensors.gatherAllData().then(data => {
-        http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
-    });
-}, 20000)
+try {
+    setInterval(() => {
+        sensors.gatherAllData().then(data => {
+            http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
+        });
+    }, 20000)
+
+} catch(e) {
+    console.log(e);
+}
