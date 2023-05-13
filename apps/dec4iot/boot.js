73440eb08608cd4cf558dbf5e1956d89042dcc88
Jakob <jakob.kampichler@gmail.com>
2023-05-14 00:27:33 +0200

    okay it didn

--- apps/dec4iot/boot.js
+++ apps/dec4iot/boot.js
@@ -1,13 +1,27 @@
-const http = require('dec4iot_lib_http');
-const sensors = require('dec4iot_lib_sensor');
+// const http = require('dec4iot_lib_http');
+// const sensors = require('dec4iot_lib_sensor');
 
-// try {
-//     setInterval(() => {
-//         sensors.gatherAllData().then(data => {
-//             http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
-//         });
-//     }, 20000)
+// // try {
+// //     setInterval(() => {
+// //         sensors.gatherAllData().then(data => {
+// //             http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
+// //         });
+// //     }, 20000)
 
-// } catch(e) {
-//     console.log(e);
-// }
+// // } catch(e) {
+// //     console.log(e);
+// // }
+
+
+const { writeDefaultConfig } = require('./config');
+
+const startSetupIntent = {t: "intent", target: "activity", action: "me.byjkdev.dec4iot.intents.banglejs.SETUP"};
+
+const config = require('dec4iot_lib_cfgman').readConfig();
+if(config === false) {
+    writeDefaultConfig();
+
+    GB(startSetupIntent)
+} else {
+
+}
