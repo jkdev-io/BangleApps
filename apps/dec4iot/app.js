951c8ab22eb1207bbfe4f9a079663b43e7a733a1
Jakob <jakob.kampichler@gmail.com>
2023-05-14 14:28:57 +0200

    Initial android app support

--- apps/dec4iot/app.js
+++ apps/dec4iot/app.js
@@ -1,23 +1,41 @@
-var http = require('dec4iot_lib_http');
-var sensors = require('dec4iot_lib_sensor');
+const http = require('dec4iot_lib_http');
+const sensors = require('dec4iot_lib_sensor');
 
 // clear the screen
 g.clear();
 g.reset().clearRect(Bangle.appRect);
 
-var configManager = require('dec4iot_lib_cfgman');
+const configManager = require('dec4iot_lib_cfgman');
+const startSetupIntent = {t: "intent", target: "activity", action: "me.byjkdev.dec4iot.intents.banglejs.SETUP", flags: ["FLAG_ACTIVITY_NEW_TASK"]};  //Sending this to Gadgetbridge will start Android App
 
-var startSetupIntent = {t: "intent", target: "activity", action: "me.byjkdev.dec4iot.intents.banglejs.SETUP", flags: ["FLAG_ACTIVITY_NEW_TASK"]};
+let config = configManager.readConfig();
 
-const config = require('dec4iot_lib_cfgman').readConfig();
-if(config === false) {
-    configManager.writeDefaultConfig();
-
-    Bluetooth.println(JSON.stringify(startSetupIntent));
-} else {
-
+function showSetupMsgs() {
+    g.setFont("6x8").setFontAlign(0, 0).drawString("Please setup your device!", g.getWidth() /2, g.getHeight() /2 - 30);
+    g.setFont("4x8").setFontAlign(0, 0).drawString("Check your phone!", g.getWidth() /2, g.getHeight() /2 + 10);
+    g.setFont("4x8").setFontAlign(0, 0).drawString("A dialogue should've opened,", g.getWidth() /2, g.getHeight() /2 + 30);
+    g.setFont("4x8").setFontAlign(0, 0).drawString("follow the steps to continue!", g.getWidth() /2, g.getHeight() /2 + 50);
 }
 
+if(config === false) {  // No config file found?
+    configManager.writeDefaultConfig();  // Write defaults
+    Bluetooth.println(JSON.stringify(startSetupIntent));  // Start setup!
+    showSetupMsgs();  // Tell user
+} else {
+
+    if(!configManager.isValidConfig(config)) {  // Config invalid; Start setup!
+        Bluetooth.println(JSON.stringify(startSetupIntent));
+        showSetupMsgs();  // Tell user
+
+    } else logic(config)  // Config valid; Start the logic!
+    
+}
+
+
+// Logic
+function logic(config) {
+    // Future big brain stuff
+}
 
 // Load widgets
 Bangle.loadWidgets();
--- apps/dec4iot/boot.js
+++ apps/dec4iot/boot.js
@@ -1,13 +1,34 @@
 var http = require('dec4iot_lib_http');
 var sensors = require('dec4iot_lib_sensor');
 
-// try {
-//     setInterval(() => {
-//         sensors.gatherAllData().then(data => {
-//             http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
-//         });
-//     }, 20000)
+var configManager = require('dec4iot_lib_cfgman');
 
-// } catch(e) {
-//     console.log(e);
-// }
+// TODO: This needs to be overhauled to support Senml!
+// TODO: This needs to be overhauled to support Senml!
+// TODO: This needs to be overhauled to support Senml!
+// TODO: This needs to be overhauled to support Senml!
+
+const config = configManager.readConfig();
+if(config === false) configManager.writeDefaultConfig()
+else {
+    let validConfig = configManager.isValidConfig(config);
+
+    if(!validConfig) {
+        // Logic here
+
+        let update_interval = config.sensor_update_interval;
+        let sensor_id = config.sensor_id;
+        let data_endpoint = config.sensor_endpoint;
+        
+        setInterval(() => {
+            try {
+                sensors.gatherAllData().then(data => {
+                    http.post("https://eo32uzgizbfichl.m.pipedream.net", data, {});
+                });
+
+            } catch(e) {
+                console.log(e);
+            }
+        }, update_interval * 1000 * 60);
+    }
+}
