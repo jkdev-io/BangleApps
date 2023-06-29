4df495322f741bb32fe9f3c04cb7f1e06fc2daef
Jakob <jakob.kampichler@gmail.com>
2023-06-29 19:46:07 +0200

    License corrected

--- apps/dec4iot/app.js
+++ apps/dec4iot/app.js
@@ -1,20 +1,25 @@
-/**
+/*
     dec4IoT on Bangle.js
-    Copyright (C) 2023 Jakob Kampichler / jkdev
+    Copyright (c) 2023 Jakob Kampichler
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <https://www.gnu.org/licenses/> or <https://choosealicense.com/licenses/gpl-3.0/>.
-**/
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 
 var sensors = require('dec4iot_lib_sensor');
 var Layout = require('Layout');
@@ -57,8 +62,8 @@ function writeDefaultConfig() {
 //#endregion  Config Functions
 
 //#region  Android
-var startSetupIntent = JSON.stringify({t: "intent", target: "activity", action: "me.byjkdev.dec4iot.intents.banglejs.SETUP", flags: ["FLAG_ACTIVITY_NEW_TASK"]});  // Sending this to Gadgetbridge will start Onboarding App
-var sendDataIntent = (data) => { return JSON.stringify({t: "intent", target: "broadcastreceiver", action: "me.byjkdev.dec4iot.intents.banglejs.SEND_DATA", "extra": { "json_data": data }, package: "run.jkdev.dec4iot.jetpack", class: "run.jkdev.dec4iot.jetpack.BangleJsDataReceiver", mimetype: "application/json"}); } // Sending this to Gadgetbridge will tell Onboarding App to send data
+const startSetupIntent = JSON.stringify({t: "intent", target: "activity", action: "me.byjkdev.dec4iot.intents.banglejs.SETUP", flags: ["FLAG_ACTIVITY_NEW_TASK"]});  // Sending this to Gadgetbridge will start Onboarding App
+const sendDataIntent = (data) => { return JSON.stringify({t: "intent", target: "broadcastreceiver", action: "me.byjkdev.dec4iot.intents.banglejs.SEND_DATA", "extra": { "json_data": data }, package: "run.jkdev.dec4iot.jetpack", class: "run.jkdev.dec4iot.jetpack.BangleJsDataReceiver", mimetype: "application/json"}); } // Sending this to Gadgetbridge will tell Onboarding App to send data
 function startLogic() {  // run by onboarding app; start logic so no app restart is needed
     g.clear();
     g.reset().clearRect(Bangle.appRect)
@@ -133,7 +138,6 @@ function logic(config) {
     });
 
     layout_start.render();
-
     
     // Load widgets
     Bangle.loadWidgets();
